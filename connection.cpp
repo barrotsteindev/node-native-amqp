bfbfda3a4af81f34c79909d4bfcf99e069426aec
Bar Rotstein <brotstein@localhost.localdomain>
2017-06-29 18:31:37 +0200

    cpp kint check

--- connection.cpp
+++ connection.cpp
@@ -22,19 +22,21 @@ class Consumer : public Nan::ObjectWrap {
   }
 
  private:
-   static inline v8::Local<v8::String> & hostnameKey() {
-       static v8::Local<v8::String> v8Hostname = Nan::New("hostname").ToLocalChecked();
-       return v8Hostname;
-    }
+  static inline v8::Local<v8::String> & hostnameKey() {
+  static v8::Local<v8::String> v8Hostname = Nan::New("hostname")
+                                                     .ToLocalChecked();
+    return v8Hostname;
+  }
 
     static const inline v8::Local<v8::String> & localhost() {
-      static v8::Local<v8::String> v8Localhost = Nan::New("localhost").ToLocalChecked();
+      static v8::Local<v8::String> v8Localhost = Nan::New("localhost")
+                                                          .ToLocalChecked();
       return v8Localhost;
     }
 
     static inline v8::Local<v8::String> & queueKey() {
-       static v8::Local<v8::String> v8Queue = Nan::New("queue").ToLocalChecked();
-       return v8Queue;
+      static v8::Local<v8::String> v8Queue = Nan::New("queue").ToLocalChecked();
+      return v8Queue;
     }
 
     static const inline v8::Local<v8::String> & ackKey() {
@@ -43,17 +45,20 @@ class Consumer : public Nan::ObjectWrap {
     }
 
     static const inline v8::Local<v8::String> & timeOutKey() {
-      static v8::Local<v8::String> v8timeOut = Nan::New("timeOut").ToLocalChecked();
+      static v8::Local<v8::String> v8timeOut = Nan::New("timeOut")
+                                                        .ToLocalChecked();
       return v8timeOut;
     }
 
     static const inline v8::Local<v8::String> & routingKey() {
-      static v8::Local<v8::String> v8RoutingKey = Nan::New("routingKey").ToLocalChecked();
+      static v8::Local<v8::String> v8RoutingKey = Nan::New("routingKey")
+                                                           .ToLocalChecked();
       return v8RoutingKey;
     }
 
     static const inline v8::Local<v8::String> & prefetchKey() {
-      static v8::Local<v8::String> v8PrefetchKey = Nan::New("prefetch").ToLocalChecked();
+      static v8::Local<v8::String> v8PrefetchKey = Nan::New("prefetch")
+                                                            .ToLocalChecked();
       return v8PrefetchKey;
     }
 
@@ -65,24 +70,33 @@ class Consumer : public Nan::ObjectWrap {
       if (!conf->Has(routingKey())) {
         Nan::ThrowTypeError("Key: routingKey must be supllied");
       }
-      v8::Local<v8::String> hostname = conf->Has(hostnameKey()) ? conf->Get(hostnameKey())->ToString() : localhost();
-      int timeOut = conf->Has(timeOutKey()) ? conf->Get(timeOutKey())->NumberValue() : 500;
-      int prefetchCount = conf->Has(prefetchKey()) ? conf->Get(prefetchKey())->NumberValue() : 10;
+      v8::Local<v8::String> hostname = conf->Has(hostnameKey()) ?
+                                       conf->Get(hostnameKey())->ToString() :
+                                       localhost();
+      int timeOut = conf->Has(timeOutKey()) ?
+                    conf->Get(timeOutKey())->NumberValue() :
+                    500;
+      int prefetchCount = conf->Has(prefetchKey()) ?
+                          conf->Get(prefetchKey())->NumberValue() :
+                          10;
       v8::Local<v8::String> v8Queue = conf->Get(queueKey())->ToString();
       v8::Local<v8::String> v8RoutingKey = conf->Get(routingKey())->ToString();
       Nan::Utf8String utfHostname(hostname);
-      hostname_ = std::string(*utfHostname);
+      _hostname = std::string(*utfHostname);
       Nan::Utf8String utfQueue(v8Queue);
       Nan::Utf8String utfRoutingKey(v8RoutingKey);
-      hostname_ = std::string(*utfHostname);
+      _hostname = std::string(*utfHostname);
       AMQPConsumer *consumer;
       try {
-          consumer = new AMQPConsumer(hostname_, std::string(*utfQueue), std::string(*utfRoutingKey), false, prefetchCount);
+          consumer = new AMQPConsumer(_hostname, std::string(*utfQueue),
+                                      std::string(*utfRoutingKey),
+                                      false, prefetchCount);
       } catch (...) {
-          std::string exceptionString = "could not connect to host: " + hostname_;
+          std::string exceptionString = "could not connect to host: "
+                                        + _hostname;
           Nan::ThrowError(exceptionString.c_str());
       }
-      consumer_ = consumer;
+      _consumer = consumer;
     }
 
   ~Consumer() {}
@@ -105,16 +119,17 @@ class Consumer : public Nan::ObjectWrap {
 
   static NAN_METHOD(Close) {
     Consumer* obj = Nan::ObjectWrap::Unwrap<Consumer>(info.Holder());
-    obj->consumer_->Close();
-    delete obj->consumer_;
+    obj->_consumer->Close();
+    delete obj->_consumer;
   }
 
   static NAN_METHOD(GetMessage) {
     Consumer* obj = Nan::ObjectWrap::Unwrap<Consumer>(info.Holder());
-    Message* msg = obj->consumer_->Poll();
+    Message* msg = obj->_consumer->Poll();
     if (!(msg->Valid())) {
       Nan::ThrowReferenceError("Consumer time out");
-      return info.GetReturnValue().Set(Nan::New<v8::String>("consumer time out").ToLocalChecked());
+      return info.GetReturnValue().Set(Nan::New<v8::String>
+                                       ("consumer time out").ToLocalChecked());
     }
     v8::Local<v8::Object> msg_obj = msg->V8Instance();
     info.GetReturnValue().Set(msg_obj);
@@ -127,7 +142,7 @@ class Consumer : public Nan::ObjectWrap {
 
   static NAN_METHOD(GetHostname) {
     Consumer* obj = Nan::ObjectWrap::Unwrap<Consumer>(info.Holder());
-    info.GetReturnValue().Set(Nan::New(obj->hostname_).ToLocalChecked());
+    info.GetReturnValue().Set(Nan::New(obj->_hostname).ToLocalChecked());
   }
 
   static inline Nan::Persistent<v8::Function> & constructor() {
@@ -135,11 +150,8 @@ class Consumer : public Nan::ObjectWrap {
     return my_constructor;
   }
 
-  std::string hostname_;
-  AMQPConsumer* consumer_;
-  Nan::Persistent<v8::Object> consumer_p;
-  AmqpClient::Envelope::ptr_t cur_msg_;
-
+  std::string _hostname;
+  AMQPConsumer* _consumer;
 };
 
 NODE_MODULE(objectwrapper, Consumer::Init)
